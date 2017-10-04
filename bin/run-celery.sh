28bce7024a2d27f64d44b329d6e062701ffff8a6
lalazari <lalazari.lalazari@uth.gr>
2017-10-04 14:47:20 +0300

    VDC1

--- bin/run-celery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-until nc -z broker 5672; do
-    echo "$(date) - waiting broker container to get up ..."
-    sleep 1
-done
-# Spawn 1 worker on the background
-celery -A celery_tasks worker
