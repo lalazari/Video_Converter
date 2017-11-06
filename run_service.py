4c07600ab5e1ec1e77bd250c4fea8a2a49f81714
lalazari <lalazari.lalazari@uth.gr>
2017-11-06 14:55:01 +0200

    update

--- run_service.py
+++ run_service.py
@@ -30,15 +30,26 @@ class Service:
         data = web.data()
         data = json.loads(data)
 
-        ###dbconn = DatabaseConnection(data)
+        if "mongodb" in data:
+            dbconn = DatabaseConnection(data)
+        else:
+            dbconn = None
 
 
         #dbconn.save({"user": data["mongodb"]["username"], "user2":"papa"})
-
         #data["mongodb"]["username"]
+        """Check if user provides input and output paths"""
+        if "input_folder" in data:
+            dir_v_path_in = data["input_folder"]
+        else:
+           dir_v_path_in = "/shared" 
 
-        dir_v_path_in = data["input_folder"]
-        dir_v_path_out = data["output_folder"]
+        if "output_folder" in data:
+            dir_v_path_out = data["output_folder"]
+        else:
+           dir_v_path_out = "/shared" 
+        
+        """Supported video types"""
         videotypes = ['.mp4', '.avi', '.wmv', '.3gp', '.flv', '.mkv', '.mpg', '.mpeg', '.ogv',
                         '.ogg', '.mov', '.qt', '.ts', '.TOD', '.MOD', '.dv4', '.h264', '.vid', '.ssf', '.sec']
         namelist = [fn for fn in os.listdir(dir_v_path_in)
@@ -49,25 +60,29 @@ class Service:
 
         response = {'input_folder': dir_v_path_in, 'output_folder': dir_v_path_out}
         processed = []
+
         for v_path in namelist:
             base_name = os.path.splitext(v_path)[0]
-            #if isfile(dir_v_path_out + '/' + v_path_out):
-                    #v_path_out = base_name + '_transcoded' #+  #'_transcoded.mp4'
-            #a_path_out = base_name + '.mp3'
 
+            if "v_out_format" not in data:
+                video_format = os.path.splitext(v_path)[1]
+                data["v_out_format"]=video_format
+            if "rotate_video" in data:
+                # Rotate Video
+                rotate_path_out = base_name + '_ROTATED_' + data["rotate_video"] + data["v_out_format"]
+            if "clip_video" in data:
+                # Video Clip / Cut part of Video
+                clip_path_out = base_name + "_Cut_From_To" + data["clip_video"]# + data["v_out_formtat"]
             # Video Convertion
             v_path_out = base_name + '_transcoded' + data["v_out_format"]
             # Create Frames, unique output name for each frame
             frame_path_out = base_name + '%03d' + '.bmp' # for the frames
             # Crop Video
             crop_path_out = base_name + '_CROPPED_' #+ data["v_out_format"]
-            # Rotate Video
-            rotate_path_out = base_name + '_ROTATED_' + data["rotate_video"] + data["v_out_format"]
+           
             # Audio Convertion
             a_path_out_wav = base_name + '.wav'
-            # Video Clip / Cut part of Video
-            clip_path_out = base_name + "_Cut_From_To" + data["clip_video"]# + data["v_out_formtat"]
-
+            
 
             v_crop = ""
             if "crop_video" in data:
@@ -100,31 +115,37 @@ class Service:
                 make_dir = subprocess.call(['mkdir', dir_v_path_out + '/' + crop_path_out + v_crop])
                 convert_video_crop.delay(dir_v_path_in, v_path, v_crop, dir_v_path_out, crop_path_out, data)
                 a_video["output_crop"] = crop_path_out + v_crop + data["v_out_format"]
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Video_crop", "Parameters":data["crop_video"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Video_crop", "Parameters":data["crop_video"], "Input_Video":base_name})
             if v_extract_frames:
                 make_dir = subprocess.call(['mkdir', dir_v_path_out + '/' + base_name + "_Frames" ])
                 convert_video_frames.delay(dir_v_path_in, v_path, dir_v_path_out, frame_path_out, base_name, data)
                 a_video["output_frames"] = frame_path_out
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Frame_Extraction", "Parameters":data["extract_frames"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Frame_Extraction", "Parameters":data["extract_frames"], "Input_Video":base_name})
             if v_transcode:
                 make_dir = subprocess.call(['mkdir', dir_v_path_out + '/' + base_name + "_transcoded"])
                 convert_video_transcode.delay(dir_v_path_in, v_path, dir_v_path_out, v_path_out, base_name)
                 a_video["output_video"] = v_path_out
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Transcode_video", "Parameters":data["v_out_format"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Transcode_video", "Parameters":data["v_out_format"], "Input_Video":base_name})
             if v_rotate:
                 make_dir = subprocess.call(['mkdir', dir_v_path_out + '/' + base_name + '_ROTATED_' + data["rotate_video"]])
                 convert_video_rotate.delay(dir_v_path_in, v_path, v_rotate, dir_v_path_out, rotate_path_out, base_name, data)
                 a_video["output_rotate"] = rotate_path_out
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Rotate_Video", "Parameters":data["rotate_video"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Rotate_Video", "Parameters":data["rotate_video"], "Input_Video":base_name})
             if v_extract_audio:
                 convert_video_audio.delay(dir_v_path_in, v_path, dir_v_path_out, a_path_out_wav)
                 a_video["output_audio"] = a_path_out_wav
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Audio_Extraction", "Parameters":data["extract_audio"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Audio_Extraction", "Parameters":data["extract_audio"], "Input_Video":base_name})
             if v_clip_video:
                 make_dir = subprocess.call(['mkdir', dir_v_path_out + '/' + clip_path_out])
                 convert_video_clip.delay(dir_v_path_in, v_path, dir_v_path_out, clip_path_out, base_name, data)
                 a_video["output_clip"] = clip_path_out + data["v_out_format"]
-                #dbconn.save({"User": data["mongodb"]["username"], "Action":"Clip_Video", "Parameters":data["clip_video"], "Input_Video":base_name})
+                if dbconn is not None:
+                    dbconn.save({"User": data["mongodb"]["username"], "Action":"Clip_Video", "Parameters":data["clip_video"], "Input_Video":base_name})
 
             processed.append(a_video)
 
